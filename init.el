34b9a6102feea3a044c22a07e9bc86cb686c67c7
JavaLearning2020 <williamalex22@126.com>
2021-04-03 16:53:49 +0800

    my emacs config

--- init.el
+++ init.el
@@ -1,3 +1,115 @@
+
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+                              ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
+
+(setq package-check-signature nil)
+;;个别包会签名验证失败
+(require 'package)
+;;初始化包管理器
+(unless (bound-and-true-p package--initialized)
+  (package-initialize))
+
+ ;;刷新软件源索引
+ (unless package-archive-contents
+    (package-refresh-contents))
+  
+ (unless (package-installed-p 'use-package)
+   (package-refresh-contents)
+   (package-install 'use-package))
+
+ ;;(eval-and-compile)
+(setq use-package-always-ensure t)
+(setq use-package-always-defer t)
+(setq use-package-always-demand nil)
+(setq use-package-expand-minimally t)
+(setq use-package-verbose t)
+
+;;packages-installed
+
+(require 'use-package)
+
+(use-package restart-emacs)
+(use-package gruvbox-theme
+  :init (load-theme 'gruvbox-dark-soft t))
+(use-package smart-mode-line
+  :init
+  (setq sml/no-confirm-load-theme t
+	sml/theme 'respectful)
+  (sml/setup))
+
+;;测试启动耗时
+(use-package benchmark-init
+  :init (benchmark-init/activate)
+  :hook (after-init . benchmark-init/deactivate))
+;;括号高亮
+(use-package rainbow-delimiters)
+(add-hook 'foo-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+;;evil-mode
+(use-package evil
+  :init
+  (evil-mode 1))
+;;company
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t
+		        company-dabbrev-code-modes t
+		        company-dabbrev-code-other-buffers 'all
+		        company-dabbrev-downcase nil
+		        company-dabbrev-ignore-case t
+		        company-dabbrev-other-buffers 'all
+		        company-require-match nil
+		        company-minimum-prefix-length 1
+		        company-show-numbers t
+		        company-tooltip-limit 20
+		        company-idle-delay 0
+		        company-echo-delay 0
+		        company-tooltip-offset-display 'scrollbar
+		        company-begin-commands '(self-insert-command))
+  (push '(company-semantic :with company-yasnippet) company-backends)
+  :hook
+  ((after-init . global-company-mode)))
+;;语法检查
+(use-package flycheck
+  :hook (after-init . global-flycheck-mode))
+
+;;user-config
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+(setq inhibit-startup-screen t)
+(setq make-backup-files nil)
+;;显示行号
+;;(global-linum-mode t)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+
+;;避免系统乱码
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(setq gc-cons-threshold most-positive-fixnum)
+
+;;keymap
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;<C-g> quit
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck evil use-package smart-mode-line restart-emacs rainbow-delimiters paredit highlight-parentheses gruvbox-theme benchmark-init))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
